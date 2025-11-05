cf7e7690c7c7e265c65ee982cb6600704490fcfd
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-11-05 07:05:26 +0000

    Remove DownloadController and associated download view

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\DownloadController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Nova\Http\Middleware\Authenticate;
-use Laravel\Nova\Nova;
 
 Route::get('/', function () {
     return redirect(route('register'));
@@ -16,12 +13,5 @@ Route::get('/buypoin', function () {
 Route::get('/dashboard', function () {
     return redirect(config('nova.path'));
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware([Authenticate::class])
-    ->prefix(Nova::path())
-    ->group(function () {
-        Route::get('/api/books/download/{filename}', [DownloadController::class, 'download'])
-            ->name('download');
-    });
 
 require __DIR__.'/auth.php';
